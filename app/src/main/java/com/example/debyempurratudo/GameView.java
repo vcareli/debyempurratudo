011d5a28100d4ab6fbcd5f5acbf2faf57d7013df
vcareli <vcareli@gmail.com>
2026-09-02 09:55:19 -0400

    inicio

--- app/src/main/java/com/example/debyempurratudo/GameView.java
+++ app/src/main/java/com/example/debyempurratudo/GameView.java
@@ -34,7 +34,10 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        int tamanhoBloco = 120;
+        tamanhoBloco = width / mapa[0].length;
+    }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
